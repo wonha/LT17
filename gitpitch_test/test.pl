6de7efad1ca915c94a218018b392dd8b948d3c18
wonha shin <wonha.shin@rakuten.com>
2017-12-07 17:51:08 +0900

    Init

---
+++ gitpitch_test/test.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+
+my @var;
+push @var, 10;
+push @var, 11;
+push @var, 12;
+push @var, 13;
+push @var, 14;
+push @var, 15;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+push @var, 16;
+
+print @var;
+
